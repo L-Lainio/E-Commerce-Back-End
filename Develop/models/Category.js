b62ae455a81cdb2c5453cbdb5dd3f909492d02ff
L-Lainio <arollainio@gmail.com>
2024-05-24 21:02:09 -0400

    defined colums in category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -5,8 +5,18 @@ const sequelize = require('../config/connection');
 class Category extends Model {}
 
 Category.init(
+  // define table columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
